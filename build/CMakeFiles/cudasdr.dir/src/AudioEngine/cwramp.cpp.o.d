e1b18d71cc8ee57551c5ea94b7e655fd5d5a364e
Simon Eatough <simon.eatough@gmail.com>
2025-09-24 14:33:02 +1200

    removed builds directorys

--- build/CMakeFiles/cudasdr.dir/src/AudioEngine/cwramp.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/cudasdr.dir/src/AudioEngine/cwramp.cpp.o: \
- /home/simon/Projects/Personal/test/cudaSDR/Source/src/AudioEngine/cwramp.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/cudasdr.dir/src/cusdr_hamDatabase.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/cudasdr.dir/src/cusdr_hamDatabase.cpp.o: \
- /home/simon/Projects/Personal/test/cudaSDR/Source/src/cusdr_hamDatabase.cpp \
- /usr/include/stdc-predef.h
